c4a0f61f082b248456956a4b1c81a8e5bc01e359
SimonWu <wuhanyu@gmail.com>
2014-03-05 13:00:44 +0800

    fix a little bug

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,6 +16,8 @@ class SongsController < ApplicationController
     @song = Song.new(params[:song].permit!)
     if @song.url == ""
       flash[:alert] = "您未上传歌曲或歌曲没有上传完毕"
+      @policy = Base64.encode64({:bucket => 'guanghe-file', :expiration => (Time.now().to_i + 3600), 'save-key' => "/sounds/{filemd5}{.suffix}","allow-file-type" =>"mp3,wav","content-length-range" => "0,20480000"}.to_json).gsub("\n","")
+      @signature = Digest::MD5.hexdigest(@policy+'&'+'kP34t27f602TN1hWsVomI0NxTXI=')
       render :new
     else
       if @song.save
