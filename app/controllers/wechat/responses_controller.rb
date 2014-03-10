a5861d602901ad2f292fc4df189e87e7055053cb
SimonWu <wuhanyu@gmail.com>
2014-03-10 13:45:57 +0800

    final fix

--- app/controllers/wechat/responses_controller.rb
+++ app/controllers/wechat/responses_controller.rb
@@ -1,27 +1,26 @@
 # coding: utf-8
-
 class Wechat::ResponsesController < Wechat::ApplicationController
   skip_before_filter :verify_authenticity_token
   before_filter :check_weixin_legality
-  
   def show
     render :text => params[:echostr]
   end
-  
+
   def create
     @action = recordlog
     if @action.type == "event"
       if @action.event == "SCAN"
         @song = Song.where(:ticket => @action.ticket).last
-	@song = Song.last if @song == nil
+        @song = Song.last if @song == nil
         render "qrcode", :formats => :xml
       end
     else
       render "echo", :formats => :xml
     end
   end
-  
+
   private
+
   # log
   def recordlog
     @wechatlog = Wechatlog.new()
@@ -38,10 +37,11 @@ class Wechat::ResponsesController < Wechat::ApplicationController
     @wechatlog.save
     return @wechatlog
   end
-  
+
   private
+
   def check_weixin_legality
     array = [Rails.configuration.wechat_token, params[:timestamp], params[:nonce]].sort
     render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
-  end 
+  end
 end
