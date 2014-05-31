c85248bd43974e88cbed0a5f8539da9deec581c4
wuhanyu <wuhanyu@gmail.com>
2014-05-31 15:20:43 +0800

    renew code

--- qrcode/randomCode.py
+++ qrcode/randomCode.py
@@ -7,7 +7,7 @@ import string
 count = 10000
 dict = {}
 while (len(dict) < count):
-	salt = ''.join(random.sample(string.ascii_letters + string.digits, 8))
+	salt = ''.join(random.sample(string.ascii_letters + string.digits, 8)).upper()
 	if (dict.has_key(salt)): break
 	dict[salt] = len(dict) % 10
 for key in dict:
